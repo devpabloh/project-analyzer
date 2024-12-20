9a29ca745326e7de9c0e175cd30962283003981f
Pablo Henrique <pablohenriqueadm94@gmail.com>
2024-12-20 13:46:36 -0300

    criando o componente de tabela

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,23 @@
 import { useState } from 'react';
 import './App.css';
 import Formulario from './Components/Formulario';
+import Tabela from './Components/Tabela';
 
 function App() {
   const [projetos, setProjetos] = useState([]) // Recebendo os projetos e lidando com suas atualizações de estado.
+  const AdicionarProjeto = ()=>{
+    setProjetos((prevProjetos)=>[...prevProjetos, projetos])
+  }
+
+  const RemoverProjeto = (id)=>{
+    setProjetos((prevProjetos)=> prevProjetos.filter((proj)=> proj.id !== id))
+  }
+
   return (
     <div className="App">
       <h1>Análise de projetos</h1>
-      <Formulario/>
+      <Formulario aoAdicionarProjeto={AdicionarProjeto}/>
+      <Tabela projetos={projetos} aoRemoverProjeto={RemoverProjeto}/>
     </div>
   );
 }
---
+++ frontend/src/Components/Tabela/index.js
@@ -0,0 +1,35 @@
+const Tabela = ({projetos, aoRemoverProjeto})=> {
+    return(
+        <table>
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Status</th>
+                    <th>Data de inicio</th>
+                    <th>Data de fim</th>
+                    <th>Riscos</th>
+                    <th>Ambiente</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {projetos.map((projeto)=> (
+                    <tr key={projeto.id}>
+                        <td>{projeto.nome}</td>
+                        <td>{projeto.status}</td>
+                        <td>{projeto.dataDeInicio}</td>
+                        <td>{projeto.dataDeFim || "N/A"}</td>
+                        <td>{projeto.riscos || "Nenhum"} </td>
+                        <td>{projeto.ambiente}</td>
+                        <td>
+                            <button onClick={()=> aoRemoverProjeto(projeto.id)}>Remover</button>
+                        </td>
+
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+}
+
+export default Tabela
